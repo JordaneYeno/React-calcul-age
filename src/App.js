3c04e9a05bc8755161126799aafc92b93ae34cd1
Jordane YENO ADEMBIGONDJO <adembigondjo@gmail.com>
2022-09-23 18:36:51 +0100

    'app'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [input, setInput] = useState("")
+  const [ageSelecteur, setAgeSelecteur] = useState("")
+
+  let Annee = (new Date()).getFullYear();
+
+  let Age = Annee - input
+  let selectYears = []
+  let resultSelect = Annee - ageSelecteur
+
+  for (let index = 1980; index <= Annee; index += 1) {
+    selectYears.push(index)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <form className='form'>
+        <label>Quelle est votre année de naissance ?</label>
+        <input id="age" name="age" required onChange={(e) => setInput(e.target.value)} />
+      </form>
+      <p> vous avez <h3>{Age}</h3></p>
+
+
+      <div className='Selector'>
+        <select name="nom" id="selector" onChange={(e) => setAgeSelecteur(e.target.value)}>
+          <option valeur="">----Selectionnez l'année-----</option>
+          {selectYears.map((year) => (
+            <option value={year}>{year}</option>
+          ))}
+        </select>
+        <p> vous avez <h3>{resultSelect}</h3></p>
+      </div>
+
+
     </div>
   );
 }
 
 export default App;
+
+
+
+
